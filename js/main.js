f0f95def54d2c138e5e1406f1d054bb43605a645
davin-go-dang <gavin.dang.goldenowl@gmail.com>
2023-05-19 14:23:36 +0700

    darkmode for header and footer

--- js/main.js
+++ js/main.js
@@ -17,3 +17,18 @@ function check_box_search()
 function write_name(){
     document.getElementById("user-name").innerHTML = 5+6;
 }
+
+//click light_mode
+function lighMode() {
+    document.getElementById("nav_bar").style.backgroundColor  =  "white";
+    document.getElementById("footer").style.backgroundColor  =  "white";
+    document.getElementsByClassName("footer-text").style.color  =  "blue";
+}
+
+
+function darkMode()
+{
+    document.getElementById("nav_bar").style.backgroundColor  =  "#3e4551";
+    document.getElementById("footer").style.backgroundColor  =  '#3e4551';
+    document.getElementsByClassName("footer-text").style.color  =  "blue";
+}
