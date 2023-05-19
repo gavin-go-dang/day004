590356ff7e60dfbef50cbe4628454dac0e45def8
davin-go-dang <gavin.dang.goldenowl@gmail.com>
2023-05-19 14:54:51 +0700

    completed darkmode

--- js/main.js
+++ js/main.js
@@ -20,9 +20,21 @@ function write_name(){
 
 //click light_mode
 function lighMode() {
-    document.getElementById("nav_bar").style.backgroundColor  =  "white";
-    document.getElementById("footer").style.backgroundColor  =  "white";
-    document.getElementsByClassName("footer-text").style.color  =  "blue";
+    document.getElementById("nav_bar").style.backgroundColor  =  "#ADD8E6";
+    document.getElementById("footer").style.backgroundColor  =  "#ADD8E6";
+    document.getElementById("body").style.backgroundColor  =  "white";
+
+    var elements = document.querySelectorAll(".footer-text");
+
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.color = "red";
+    }
+
+    var elements = document.querySelectorAll(".header-text");
+
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.color = "red";
+    }
 }
 
 
@@ -30,5 +42,18 @@ function darkMode()
 {
     document.getElementById("nav_bar").style.backgroundColor  =  "#3e4551";
     document.getElementById("footer").style.backgroundColor  =  '#3e4551';
-    document.getElementsByClassName("footer-text").style.color  =  "blue";
+    document.getElementById("body").style.backgroundColor  =  "gray";
+
+    var elements = document.querySelectorAll(".footer-text");
+
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.color = "blue";
+    }
+
+    var elements = document.querySelectorAll(".header-text");
+
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.color = "blue";
+    }
 }
+
