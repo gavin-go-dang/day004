36642592c3f3af235a1b2ae2eab560fc6cce8704
davin-go-dang <gavin.dang.goldenowl@gmail.com>
2023-05-19 09:09:01 +0700

    copy from day002

---
+++ README.md
@@ -0,0 +1,1 @@
+# day002_html_css
---
+++ js/main.js
@@ -0,0 +1,10 @@
+check()
+{
+    if (!frm1.FileName.value)
+                {
+                    alert ("Please Enter a File Name");
+                    return (false);
+                }
+                return alert (frm1.FileName.value);;
+}
+
